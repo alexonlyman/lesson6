6536bc654825d2a0d6a090a5ee03a30a7b6207fe
Алекс <chu@mail.ru>
2022-10-01 14:59:59 +0300

    lesson6task5

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
         public static void main (String[] args){
 
 
-        boolean clientOS = false;
-                 if(clientOS)
+        int clientOS = 1;
+                 if(clientOS==1)
         {
                 System.out.println("Установите версию IOS по ссылке");
         }
@@ -13,12 +13,12 @@ public class Main {
 
         }
         // задача 2
-                int clientDeviceYear = 2014;
-                 if (clientDeviceYear<=2015 && clientOS){
+                int clientDeviceYear = 2019;
+                 if (clientDeviceYear<=2015 && clientOS ==1){
                          System.out.println("Установи облегченную версию для IOS");
-                 }else if (clientDeviceYear>2015 && clientOS) {
+                 }else if (clientDeviceYear>2015 && clientOS==1) {
                          System.out.println("Установите приложение для IOS");
-                } else if (clientDeviceYear<=2015&&clientOS==false){
+                } else if (clientDeviceYear<=2015&&clientOS!=1){
                      System.out.println("Установи облегченную версию для Android");
                  } else {System.out.println("Установите приложение для Android");}
         // задача 3
@@ -48,8 +48,33 @@ public class Main {
             }
 
 
+// задача 5
 
-
+            int monthNumber = 3;
+            switch (monthNumber){
+                case 1:
+                case 2:
+                case 3:
+                    System.out.println("Зима");
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    System.out.println("весна");
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                    System.out.println("Лето");
+                    break;
+                case 10:
+                case 11:
+                case 12:
+                    System.out.println("Осень");
+                    break;
+                default:
+                    System.out.println("Больше времен года нет");
+            }
 
         }
 
