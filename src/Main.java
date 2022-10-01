86d5804e8cf67549b727e930508a143c2dda5219
Алекс <chu@mail.ru>
2022-10-01 13:18:57 +0300

    lesson6task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-        public static void main (String args[]){
+        public static void main (String[] args){
+
+
+        boolean clientOS = false;
+        if(clientOS)
+        {
+                System.out.println("Установите версию IOS по ссылке");
+        }else
+        {       System.out.println("Установите версию Amdroid по ссылке ");
+
+        }
+        // задача 2
+
+
+
+
+
 
 
         }
