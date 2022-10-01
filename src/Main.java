beb0bd3f105f1cad83acd943c13cffe7cfae92a7
Алекс <chu@mail.ru>
2022-10-01 14:26:33 +0300

    lesson6task3

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,21 @@ public class Main {
                 } else if (clientDeviceYear<=2015&&clientOS==false){
                      System.out.println("Установи облегченную версию для Android");
                  } else {System.out.println("Установите приложение для Android");}
+        // задача 3
 
+
+                int year = 2020;
+                 switch (year){
+                     case 2017:
+                         System.out.println("Год не является високосным");
+                         break;
+                     case 2018:
+                         System.out.println("Год является високосным");
+                         break;
+                     default:
+                         System.out.println("Больше годов нет");
+                 }
+                 //задача 4
 
 
 
